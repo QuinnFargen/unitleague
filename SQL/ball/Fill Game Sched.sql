f457a00336c0a255c256645c6ddde30a6c2d2ec7
Quinn Fargen <quinnfargen@gmail.com>
2025-08-15 08:17:49 -0500

    game and sched filled with is pre & post added

--- SQL/ball/Fill Game Sched.sql
+++ SQL/ball/Fill Game Sched.sql
@@ -1,4 +1,7 @@
 
+
+
+
 select * from src.foot_schedule fs2 ;
 
 select * from ball.game g ;
@@ -34,11 +37,91 @@ order by g.game_dt desc;
 
 --NBA_23_24_LAL_###_OKC
 select * from ball.sched s ;
+select * from ball.season s order by s.reg_end_dt DESC;
+
+--insert into ball.season (season_id,league_id,yr_var,season_concat,reg_start_dt,champ_dt,yr)
+--select 202025,2,'2025','NFL_2025','2025-09-04'::date,'2026-02-08'::date,2025
+--union
+--select 502025,5,'2025','CFB_2025','2025-08-23'::date,'2026-01-19'::date,2025
+--;
 
 
+--SELECT COUNT(*), g.game_id 
+--from ball.game g 	-- 14923
+--join ball.season s on g.league_id = s.league_id and g.game_dt between s.reg_start_dt - interval '2 month' and s.champ_dt + interval '2 month'
+--group by g.game_id 
+--having COUNT(*) > 1;
 
 
+insert into ball.sched (league_id,season_id,team_id,game_num,opp_team_id,sched_concat,game_dt,home, won,team, opp, game_id, is_pre, is_post)
+select 
+	g.league_id, s.season_id
+	, g.home_team_id 
+	,1 as game_num -- will need to update after
+	, g.away_team_id 
+	,s.season_concat -- will need to update affter
+	,g.game_dt 
+	,true as home
+	,case when g.won_team_id = g.home_team_id then true else false end as won
+	,g.h as team
+	,g.a as opp
+	,g.game_id 
+	,case when g.game_dt < s.reg_start_dt then true else false end as is_pre
+	,case when g.game_dt > s.reg_end_dt then true else false end as is_post
+--	,g.*
+	-- SELECT COUNT(*)
+from ball.game g 	-- 14923
+join ball.season s on g.league_id = s.league_id and g.game_dt between s.reg_start_dt - interval '2 month' and s.champ_dt + interval '2 month'
+where 1=1
+	and g.home_team_id not in (20000,50000)
+--	and s.season_id is null 
+	and g.game_dt > '2011-07-05'	-- 14591
+--order by g.game_dt 
+	
+union
 
+select 
+	g.league_id, s.season_id
+	, g.away_team_id 
+	,1 as game_num -- will need to update after
+	, g.home_team_id 
+	,s.season_concat -- will need to update affter
+	,g.game_dt 
+	,false as home
+	,case when g.won_team_id = g.away_team_id then true else false end as won
+	,g.a as team
+	,g.h as opp
+	,g.game_id 
+	,case when g.game_dt < s.reg_start_dt then true else false end as is_pre
+	,case when g.game_dt > s.reg_end_dt then true else false end as is_post
+--	,g.*
+	-- SELECT COUNT(*)
+from ball.game g 	-- 14923
+join ball.season s on g.league_id = s.league_id and g.game_dt between s.reg_start_dt - interval '2 month' and s.champ_dt + interval '2 month'
+where 1=1
+	and g.away_team_id not in (20000,50000)
+--	and s.season_id is null 
+	and g.game_dt > '2011-07-05'	-- 14591
+	
+;-- 27962
 
+select * from ball.sched s ;
+
+--NBA_23_24_LAL_###_OKC
+
+UPDATE ball.sched
+set sched_concat = a.sched_concat
+	,game_num = a.game_num  
+from (
+	select s.sched_id
+		,s.sched_concat || '_' || t.abbr || '_' || (row_number() OVER(partition by s.season_id, s.team_id, s.is_pre, s.is_post order by s.GAME_DT ))::varchar 
+						|| '_' || o.abbr || case when is_post then '_POST' when is_pre then '_PRE' else '' end as SCHED_CONCAT
+		,row_number() OVER(partition by s.SEASON_ID, s.team_id, s.is_pre, s.is_post order by s.GAME_DT ) as GAME_NUM
+	from ball.sched s
+	join ball.team t on s.team_id  = t.team_id 
+	join ball.team o on s.opp_team_id  = o.team_id 
+--	where s.season_id   = 202024 and s.team_id   = 22503
+	) a
+where ball.sched.sched_id  = a.sched_id;
 
 

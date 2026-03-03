bfceb4b42ffb70649c76a86b5fd9594d8be32820
Quinn Fargen <quinnfargen@gmail.com>
2026-03-02 22:27:24 -0600

    new season values, seed game & sched procedure for all leagues

--- db/functions/utility/seed_ball_game_sched.sql
+++ db/functions/utility/seed_ball_game_sched.sql
@@ -4,29 +4,52 @@ AS $$
 BEGIN
 
 -----------------------
--- Load ball.game from src.foog_schedule
+-- Load ball.game from src.foot_schedule
 
 	truncate table ball.game;
 
 	insert into ball.game (league_id, home_team_id, away_team_id, game_dt, game_time, h, a, won_team_id, source_game_id, game_concat, 
-							a1, a2, a3, a4, a5, h1, h2, h3, h4, h5	)					
+							a1, a2, a3, a4, a5, a6, a7, a8, a9, a10,
+							h1, h2, h3, h4, h5, h6, h7, h8, h9, h10	)					
 	select 
 		fs2.league_id, coalesce(t.team_id,(fs2.league_id * 10000)), coalesce(a.team_id,(fs2.league_id * 10000)), fs2.gamedate::date, fs2.gamedate::time, fs2.home_score, fs2.away_score
 		,case 	when fs2.home_score = 0 and fs2.away_score = 0 then null
 				when fs2.home_score > fs2.away_score then coalesce(t.team_id,(fs2.league_id * 10000)) 
 				else coalesce(a.team_id,(fs2.league_id * 10000)) end
 		,(fs2.game_id) as source_game_id
-		,coalesce(t.team_concat,'CFB_TBD') || '_' || coalesce(a.abbr,'TBD') || '_' || TO_CHAR(fs2.gamedate, 'YYYYMMDD')
-		,fs2.q1_away,fs2.q2_away,fs2.q3_away,fs2.q4_away,case when fs2.q5_away = 0 and fs2.q5_home = 0 then null else fs2.q5_away end
-		,fs2.q1_home ,fs2.q2_home ,fs2.q3_home ,fs2.q4_home ,case when fs2.q5_away = 0 and fs2.q5_home = 0 then null else fs2.q5_home end
+		,coalesce(t.team_concat, l.name || '_TBD') || '_' || coalesce(a.abbr,'TBD') || '_' || TO_CHAR(fs2.gamedate, 'YYYYMMDD')
+		,case when fs2.a1 = 0 and fs2.a1 = 0 then null else fs2.a1 end as a1
+		,case when fs2.a2 = 0 and fs2.a2 = 0 then null else fs2.a2 end as a2
+		,case when fs2.a3 = 0 and fs2.a3 = 0 then null else fs2.a3 end as a3
+		,case when fs2.a4 = 0 and fs2.a4 = 0 then null else fs2.a4 end as a4
+		,case when fs2.a5 = 0 and fs2.a5 = 0 then null else fs2.a5 end as a5
+		,case when fs2.a6 = 0 and fs2.a6 = 0 then null else fs2.a6 end as a6
+		,case when fs2.a7 = 0 and fs2.a7 = 0 then null else fs2.a7 end as a7
+		,case when fs2.a8 = 0 and fs2.a8 = 0 then null else fs2.a8 end as a8
+		,case when fs2.a9 = 0 and fs2.a9 = 0 then null else fs2.a9 end as a9
+		,case when fs2.a10 = 0 and fs2.a10 = 0 then null else fs2.a10 end as a10
+
+		,case when fs2.h1 = 0 and fs2.h1 = 0 then null else fs2.h1 end as h1
+		,case when fs2.h2 = 0 and fs2.h2 = 0 then null else fs2.h2 end as h2
+		,case when fs2.h3 = 0 and fs2.h3 = 0 then null else fs2.h3 end as h3
+		,case when fs2.h4 = 0 and fs2.h4 = 0 then null else fs2.h4 end as h4
+		,case when fs2.h5 = 0 and fs2.h5 = 0 then null else fs2.h5 end as h5
+		,case when fs2.h6 = 0 and fs2.h6 = 0 then null else fs2.h6 end as h6
+		,case when fs2.h7 = 0 and fs2.h7 = 0 then null else fs2.h7 end as h7
+		,case when fs2.h8 = 0 and fs2.h8 = 0 then null else fs2.h8 end as h8
+		,case when fs2.h9 = 0 and fs2.h9 = 0 then null else fs2.h9 end as h9
+		,case when fs2.h10 = 0 and fs2.h10 = 0 then null else fs2.h10 end as h10
+
 	--	,t.abbr , a.abbr , fs2.shortname, fs2.*
-	-- SELECT COUNT(*)
-	from src.foot_espn_schedule fs2 	-- 14940
+	-- SELECT COUNT(*)	-- select fs2.*
+	from src.espn_schedule fs2 	-- 94065
+	join ball.league l on fs2.league_id = l.league_id
 	left join ball.v_team t on fs2.league_id = t.league_id and fs2.home_team_id = t.espn_team_id
 	left join ball.v_team a on fs2.league_id = a.league_id and fs2.away_team_id = a.espn_team_id
 	where t.team_id  is not null or a.team_id is not null	-- 14923
-	order by fs2.game_id desc
-	;
+			-- lose some to pro all stars, tbd or weird college basketball that we don't have the team yet for
+--	order by fs2.game_id desc
+	;	-- 92789
 
 -----------------------
 -- Load ball.sched from ball.game
@@ -49,13 +72,10 @@ BEGIN
 	--	,g.*
 		-- SELECT COUNT(*)
 	from ball.game g 	-- 14923
-	join ball.season s on g.league_id = s.league_id and g.game_dt between s.reg_start_dt - interval '2 month' and s.champ_series_start_dt + interval '2 month'
+	join ball.season s on g.league_id = s.league_id and g.game_dt between s.reg_start_dt - interval '2 month' and s.champ_dt + interval '2 month'
 	where 1=1
-		and g.home_team_id not in (20000,50000)
-	--	and s.season_id is null 
-		and g.game_dt > '2011-07-05'	-- 14591
-	--order by g.game_dt 
-		
+		and g.home_team_id % 10000 <> 0	-- Not unknown team
+
 	union
 	
 	select 
@@ -73,13 +93,11 @@ BEGIN
 	--	,g.*
 		-- SELECT COUNT(*)
 	from ball.game g 	-- 14923
-	join ball.season s on g.league_id = s.league_id and g.game_dt between s.reg_start_dt - interval '2 month' and s.champ_series_start_dt + interval '2 month'
+	join ball.season s on g.league_id = s.league_id and g.game_dt between s.reg_start_dt - interval '2 month' and s.champ_dt + interval '2 month'
 	where 1=1
-		and g.away_team_id not in (20000,50000)
-	--	and s.season_id is null 
-		and g.game_dt > '2011-07-05'	-- 14591
-		
+		and g.away_team_id % 10000 <> 0	-- Not unknown team		
 	;-- 27962
+
 
 	UPDATE ball.sched
 	set sched_concat = a.sched_concat

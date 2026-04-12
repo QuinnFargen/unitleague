206005f8409facf8b1a93dfd05fe48b12e932889
Quinn Fargen <quinnfargen@gmail.com>
2026-04-12 13:16:16 -0500

    future schedule won't have scores

--- db/views/src/v_espn_schedule_future.sql
+++ db/views/src/v_espn_schedule_future.sql
@@ -15,31 +15,11 @@ AS
 		,a.home_abbr
 		,a.home_short
 		,a.home_team_id
-		,a.h1
-		,a.h2
-		,a.h3
-		,a.h4
-		,a.h5
-		,a.h6
-		,a.h7
-		,a.h8
-		,a.h9
-		,a.h10
 		,a.away_score
 		,a.away_team
 		,a.away_abbr
 		,a.away_short
 		,a.away_team_id
-		,a.a1
-		,a.a2
-		,a.a3
-		,a.a4
-		,a.a5
-		,a.a6
-		,a.a7
-		,a.a8
-		,a.a9
-		,a.a10
 	from etl.stg_espn_schedule_future a
 	where not exists (
 		select 1
